4a2bdd545396b640043235a6cf873375a9007ba4
Amer-Morsy <amermousa341@gmail.com>
2024-09-05 16:18:38 +0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <UpperBanner />
+    <TheFeatures />
+  </div>
+</template>
+
+<script>
+import TheFeatures from "@/components/home_page/TheFeatures.vue";
+import UpperBanner from "@/components/home_page/UpperBanner.vue";
+
+export default {
+  name: "HomeView",
+  components: { UpperBanner, TheFeatures },
+};
+</script>
